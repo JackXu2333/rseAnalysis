d23ad3d02ab8a4f96ef231c9b33632ab95f83dda
jackxu <sijie.xu@easygroup.ca>
2020-11-20 21:21:08 -0500

    remove Vig folder

--- Vig/Untitled.R
+++
@@ -1,6 +0,0 @@
-#Generate mutated RNA based on the fasta, bed and vcf file, select only the validated
-RNA.mutate <- RNA.validate(fasta = fasta, vcf = vcf, bed = bed)
-RNA.mutate <- subset(RNA.mutate, MATCH == TRUE)
-
-#Perform secondary structure prediction
-ori.structure <- predict.Structure(name = RNA.mutate$NAME, seq = RNA.mutate$SEQ)
